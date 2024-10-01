39dc440d5f8f3d1246e123cdc09795891554ce6e
anselme333 <anselme333@gmail.com>
2024-09-30 22:18:17 -0400

    add change for module 1

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns = [ path('contact/', TemplateView.as_view(template_name="Contact.html")),path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
